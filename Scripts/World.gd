5cf51c636cd47b819e3968f7e48c72e62c84cf26
hrong1 <hrong1@ucsc.edu>
2024-05-17 08:31:10 -0700

    small fix

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -7,7 +7,7 @@ var instance
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
